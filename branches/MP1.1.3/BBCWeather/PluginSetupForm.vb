135bc34d52ad21a0b1fdd7825a8c5a9685ff6f0a
martinvanderboon@gmail.com <martinvanderboon@gmail.com@36d24fcd-a24c-8e49-6ac7-2b042af20af5>
2011-07-16 05:58:58 +0000

    updates for v0.3

--- branches/MP1.1.3/BBCWeather/PluginSetupForm.vb
+++ branches/MP1.1.3/BBCWeather/PluginSetupForm.vb
@@ -16,17 +16,41 @@ Public Class PluginSetupForm
     Private _ja As JsonArray
     Private _areaCode As String = String.Empty
     Private _areaName As String = String.Empty
+    Private _tempUnit As String = String.Empty
+    Private _windUnit As String = String.Empty
+    Private _overRide As Boolean = False
 
     Private Sub PluginSetupForm_Load(sender As System.Object, e As System.EventArgs) Handles MyBase.Load
 
         AppDomain.CurrentDomain.AppendPrivatePath(String.Format("{0}\Plugins\Windows\BBCWeather", AppDomain.CurrentDomain.BaseDirectory))
 
         Using xmlReader As Settings = New Settings(Config.GetFile(Config.Dir.Config, "MediaPortal.xml"))
-            _areaCode = xmlReader.GetValue("BBCWeather", "areaCode")
-            _areaName = xmlReader.GetValue("BBCWeather", "areaName")
+            _areaCode = xmlReader.GetValueAsString("BBCWeather", "areaCode", "8")
+            _areaName = xmlReader.GetValueAsString("BBCWeather", "areaName", "London")
+            _tempUnit = xmlReader.GetValueAsString("BBCWeather", "tempUnit", "degC")
+            _windUnit = xmlReader.GetValueAsString("BBCWeather", "windUnit", "mph")
+            _overRide = xmlReader.GetValueAsBool("BBCWeather", "overRide", False)
         End Using
 
         If _areaName.Length > 0 Then AreaLookup(_areaName)
+
+        If _tempUnit.ToLower = "degc" Then
+            rbnDegC.Checked = True
+            rbnDegF.Checked = False
+        Else
+            rbnDegC.Checked = False
+            rbnDegF.Checked = True
+        End If
+
+        If _windUnit.ToLower = "mph" Then
+            rbnMph.Checked = True
+            rbnKph.Checked = False
+        Else
+            rbnMph.Checked = False
+            rbnKph.Checked = True
+        End If
+
+        cbxInfoService.Checked = _overRide
 
     End Sub
 
@@ -86,13 +110,20 @@ Public Class PluginSetupForm
             _areaCode = _ja(3)(clbResults.Items.IndexOf(checkedItem))
             _areaName = _ja(1)(clbResults.Items.IndexOf(checkedItem))
         Next
+
         If _areaCode <> "" Then
             Using xmlReader As Settings = New Settings(Config.GetFile(Config.Dir.Config, "MediaPortal.xml"))
                 xmlReader.SetValue("BBCWeather", "areaCode", _areaCode)
                 xmlReader.SetValue("BBCWeather", "areaName", _areaName)
+                xmlReader.SetValue("BBCWeather", "tempUnit", IIf(rbnDegF.Checked, "degF", "degC"))
+                xmlReader.SetValue("BBCWeather", "windUnit", IIf(rbnKph.Checked, "kph", "mph"))
+                xmlReader.SetValueAsBool("BBCWeather", "overRide", cbxInfoService.Checked)
                 Settings.SaveCache()
             End Using
         End If
+
+        If cbxInfoService.Checked Then MessageBox.Show("You should open InfoService config now and remove the tick from the 'Weather information enabled' checkbox on the Weather tab. Then click Save.", "BBC Weather", MessageBoxButtons.OK, MessageBoxIcon.Information)
+
         Me.Close()
     End Sub
 
